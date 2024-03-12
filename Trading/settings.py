880fd848281c8e6170b797c4f076e3c9f7ce4e92
Mrinal-Mahajan <mahajanmrinal2013@gmail.com>
2024-03-12 06:08:53 +0530

    just one more change

--- Trading/settings.py
+++ Trading/settings.py
@@ -132,8 +132,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend',
-    'allauth.account.auth_backends.AuthenticationBackend'
+    'django.contrib.auth.backends.ModelBackend'
 )
 
 SITE_ID = 2
