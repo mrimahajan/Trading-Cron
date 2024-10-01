245174d65a862d9c06c82682fb92268b34fbd114
Mrinal Mahajan <mahajanmrinal2013@gmail.com>
2024-10-01 15:07:26 +0530

    Create update_data_nse_mnc.py

---
+++ update_data_nse_mnc.py
@@ -0,0 +1,30 @@
+import os
+import django
+import datetime
+from datetime import date
+import numpy as np 
+import yfinance as yf
+import pandas as pd
+from decimal import Decimal
+import gc
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Trading.settings')
+django.setup() 
+
+from Broker.models import Stock
+
+Name="NSE"
+sector = "Multi Nationals"
+
+for stock in Stock.objects.filter(Exchange__Name=Name).filter(Sector=sector):
+  symbol = stock.Symbol
+  df = yf.download(tickers=symbol, period='1d', interval='1m')
+  try:
+    last_price = Decimal(np.round(list(df['Close'])[-1], 2))
+    del df
+    gc.collect()
+    stock.EOD_Price = last_price
+    stock.save()
+  except:
+    del df
+    gc.collect()
