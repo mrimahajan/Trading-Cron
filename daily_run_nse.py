acb9e70780158266cb46936975aa4e9278370c17
Mrinal Mahajan <mahajanmrinal2013@gmail.com>
2024-10-03 23:46:04 +0530

    Update daily_run_nse.py

--- daily_run_nse.py
+++ daily_run_nse.py
@@ -319,9 +319,9 @@ def run_daily():
             if len(train_dates) > 0:
                 model.save(staticfiles_storage.path(f'NSE/Models/Classification/{sector}_model.h5'))
                 equation_new = get_equation(model)
+                equation_df = pd.DataFrame([[str(train_dates[-1])]+equation_new],columns=['Date']+input_cols+['intercept'])
                 old_data = pd.read_excel(staticfiles_storage.path(f'NSE/Models/Classification/Equation/{sector}.xlsx'),header=0)
                 equation_df = pd.concat([old_data,equation_df],axis=0)
-                equation_df = pd.DataFrame([[str(train_dates[-1])]+equation_new],columns=['Date']+input_cols+['intercept'])
                 equation_df.to_excel(staticfiles_storage.path(f'NSE/Models/Classification/Equation/{sector}.xlsx'),index=False)
         
         
